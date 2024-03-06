452344ff15036ea891fbf4adc2b4719cab1310ca
Soong-moo <blueday0214@gmail.com>
2024-03-06 15:32:42 +0900

    FP-26 feat : MongoDB 연동

---
+++ src/main/java/com/imp/fluffy_mood/config/MongoDBConfig.java
@@ -0,0 +1,32 @@
+package com.imp.fluffy_mood.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackageClasses = com.imp.fluffy_mood.repository.SensorRepository.class)
+public class MongoDBConfig {
+
+    private final MongoMappingContext mongoMappingContext;
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDatabaseFactory,
+                                                       MongoMappingContext mongoMappingContext) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+
+}
