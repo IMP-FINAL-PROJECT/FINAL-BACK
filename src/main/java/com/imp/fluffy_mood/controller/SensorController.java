1e89ec621e6509cdfb6c476c11ebd1500531e568
Soong-moo <blueday0214@gmail.com>
2024-03-14 16:34:24 +0900

    FP-69 feat : log4j2 추가

--- src/main/java/com/imp/fluffy_mood/controller/SensorController.java
+++ src/main/java/com/imp/fluffy_mood/controller/SensorController.java
@@ -3,10 +3,13 @@ package com.imp.fluffy_mood.controller;
 import com.imp.fluffy_mood.dto.SensorDto;
 import com.imp.fluffy_mood.entity.Message;
 import com.imp.fluffy_mood.service.SensorService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
+@Slf4j
 @RestController
 @RequestMapping("/api/sensor")
 public class SensorController {
@@ -18,12 +21,17 @@ public class SensorController {
     }
 
     @PostMapping("/insert")
-    public ResponseEntity<Message> insert(@RequestBody SensorDto sensorDto) {
+    public ResponseEntity<Message> insert(HttpServletRequest request, @RequestBody SensorDto sensorDto) {
+        log.debug("Accessed IP : {}", request.getRemoteAddr());
+        log.debug("id : {}, illuminance : {}, pedometer : {}, screen_frequency : {}, screen_duration : {}, gps : {}, timestamp : {}, hour : {}",
+                sensorDto.getId(), sensorDto.getIlluminance(), sensorDto.getPedometer(), sensorDto.getScreen_frequency(), sensorDto.getScreen_duration(), sensorDto.getTimestamp(), sensorDto.getHour());
         return sensorService.insert(sensorDto);
     }
 
     @GetMapping("/test")
-    public ResponseEntity<Message> test(@RequestBody SensorDto sensorDto) {
+    public ResponseEntity<Message> test(HttpServletRequest request, @RequestBody SensorDto sensorDto) {
+        log.debug("Accessed IP : {}", request.getRemoteAddr());
+        log.debug("id : {}", sensorDto.getId());
         return sensorService.test(sensorDto);
     }
 
--- src/main/java/com/imp/fluffy_mood/controller/UserController.java
+++ src/main/java/com/imp/fluffy_mood/controller/UserController.java
@@ -3,10 +3,13 @@ package com.imp.fluffy_mood.controller;
 import com.imp.fluffy_mood.dto.UserDto;
 import com.imp.fluffy_mood.entity.Message;
 import com.imp.fluffy_mood.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class UserController {
@@ -18,22 +21,31 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Message> login(@RequestBody UserDto userDto) {
+    public ResponseEntity<Message> login(HttpServletRequest request, @RequestBody UserDto userDto) {
+        log.debug("Accessed IP : {}", request.getRemoteAddr());
+        log.debug("id : {}", userDto.getId());
         return userService.login(userDto);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody UserDto userDto) {
+    public ResponseEntity<Message> register(HttpServletRequest request, @RequestBody UserDto userDto) {
+        System.out.println(userDto.getBirth());
+        log.debug("Accessed IP : {}", request.getRemoteAddr());
+        log.debug("id : {}, birth : {}, name : {}, gender : {}", userDto.getId(), userDto.getBirth(), userDto.getName(), userDto.getGender());
         return userService.register(userDto);
     }
 
     @GetMapping("/register/validation")
-    public ResponseEntity<?> idValidation(@RequestParam String id) {
+    public ResponseEntity<Message> idValidation(HttpServletRequest request, @RequestParam String id) {
+        log.debug("Accessed IP : {}", request.getRemoteAddr());
+        log.debug("id : {}", id);
         return userService.idValidation(id);
     }
 
     @PutMapping("/change/{id}")
-    public ResponseEntity<Message> changeInfo(@PathVariable String id, @RequestBody UserDto userDto) {
+    public ResponseEntity<Message> changeInfo(HttpServletRequest request, @PathVariable String id, @RequestBody UserDto userDto) {
+        log.debug("Accessed IP : {}", request.getRemoteAddr());
+        log.debug("id : {}, name : {}, birth : {}, gender : {}", id, userDto.getName(), userDto.getBirth(), userDto.getGender());
         return userService.changeInfo(id, userDto);
     }
 }
