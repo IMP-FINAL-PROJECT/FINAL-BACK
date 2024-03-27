cebf1e46523e16e77bb3500a8f395e23c4a634ea
Soong-moo <blueday0214@gmail.com>
2024-03-27 21:01:07 +0900

    FP-91 feat : 센서 index column 추가

--- src/main/java/com/imp/fluffy_mood/repository/SensorRepository.java
+++ src/main/java/com/imp/fluffy_mood/repository/SensorRepository.java
@@ -1,7 +1,6 @@
 package com.imp.fluffy_mood.repository;
 
 import com.imp.fluffy_mood.entity.Sensor;
-import com.imp.fluffy_mood.entity.pk.SensorPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +8,7 @@ import java.time.LocalDate;
 
 
 @Repository
-public interface SensorRepository extends JpaRepository<Sensor, SensorPK> {
+public interface SensorRepository extends JpaRepository<Sensor, Integer> {
 
     Sensor findByIdAndTimestampAndHour(String id, LocalDate timestamp, int hour);
 }
