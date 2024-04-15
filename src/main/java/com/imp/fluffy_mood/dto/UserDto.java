96125c1d5046da77916957ce810a70288ea2086b
Soong-moo <blueday0214@gmail.com>
2024-04-15 15:57:43 +0900

    FP-166 feat : 사용자 마지막 로그인 날짜 추가

--- src/main/java/com/imp/fluffy_mood/dto/UserDto.java
+++ src/main/java/com/imp/fluffy_mood/dto/UserDto.java
@@ -1,8 +1,10 @@
 package com.imp.fluffy_mood.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.imp.fluffy_mood.entity.User;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -19,6 +21,8 @@ public class UserDto {
     private String name;
     private Character gender;
     private List<? extends Number> address;
+    @JsonProperty("last_login")
+    private LocalDateTime lastLogin;
 
     public User toEntity() {
         return User.builder()
@@ -28,6 +32,7 @@ public class UserDto {
                 .name(name)
                 .gender(gender)
                 .address(address)
+                .lastLogin(lastLogin)
                 .build();
     }
 
