21e42d86c72aa7bf6b1729cf9122cceb9cbb7d4d
Soong-moo <blueday0214@gmail.com>
2024-05-25 02:04:43 +0900

    FP-342 feat : grafana monitoring 추가

--- src/main/java/com/imp/fluffy_mood/config/SecurityConfig.java
+++ src/main/java/com/imp/fluffy_mood/config/SecurityConfig.java
@@ -2,10 +2,26 @@ package com.imp.fluffy_mood.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/actuator").permitAll()
+                .anyRequest().authenticated()
+        ).httpBasic(withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
